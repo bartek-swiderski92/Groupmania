450e22306528cfd93a39c7a477448fb573ebb893
Phos23 <phos23@gmail.com>
2021-10-06 20:22:51 +0100

    add post routes

--- app.js
+++ app.js
@@ -3,25 +3,22 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const userRoutes = require('./routes/user')
+const postRoutes = require('./routes/posts.js')
 
 //Database
 const db = require('./config/database')
 
-// Test DB
 db.authenticate()
     .then(() => console.log('Database connected...'))
     .catch(err => console.log('Error: ' + err));
 
-
+// APP
 const app = express();
-
 app.get('/', (req, res) => res.send('INDEX'))
-
-// Post routes
-app.use('/posts', require('./routes/posts.js'))
-
 const PORT = process.env.PORT || 5000;
 
+// Routes
+app.use('/posts', postRoutes)
 app.use('/users', userRoutes)
 
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+app.listen(PORT, console.log(`Server is listening at http://localhost:${PORT}/`));
