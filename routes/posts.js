1a227550ed724ac5dab308aadbfe701c96fabc05
Phos23 <phos23@gmail.com>
2021-10-06 22:16:47 +0100

    add routes

--- routes/posts.js
+++ routes/posts.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../config/database');
-const Post = require('../models/post');
 
-router.get('/', (req, res) => Post.findAll()
-    .then(posts => {
-        console.log(posts)
-        res.sendStatus(200)
-    })
-    .catch(err => console.log(err))
-);
+const auth = '';
+const multer = '';
+
+const postCtrl = require('../controllers/posts')
+
+router.get('/', postCtrl.getAllPosts);
 
 module.exports = router;
