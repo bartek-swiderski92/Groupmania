09c9e02a73722d49a7f5a98cea8388ec68829847
Phos23 <phos23@gmail.com>
2021-10-06 21:56:58 +0100

    add todo

--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const userCtrl = require('../controllers/user')
 
+//TODO: does it make sense? VV
 const users = express.Router();
 const cors = require('cors');
 
+//TODO: does it make sense? VV
 users.use(cors());
 
 process.env.SECRET_KEY = 'secret';
