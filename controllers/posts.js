191b1480efdef5c2916b98171c508d5774a23763
Phos23 <phos23@gmail.com>
2021-10-07 13:27:17 +0100

    add display, create and delete methods

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,17 +1,63 @@
 const Post = require('../models/post');
 const fs = require('fs');
+const { secureHeapUsed } = require('crypto');
 
 //TODO: Z_FIXED?
 // const {
 //     Z_FIXED
 // } = require('zlib');
 
-exports.getAllPosts = (req,res,next) =>{
-    Post.find().then((posts)=>{
-        res.status(200).json(posts);
+exports.getAllPosts = (req, res, next) => {
+    Post.find().then((posts) => {
+            res.status(200).json(posts);
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(400).json({
+                error: error
+            })
+        })
+}
+
+exports.getOnePost = (req, res, next) => {
+    Post.findOne({
+        _id: req.params.id
+    }).then((post) => {
+        res.status(200).json(post);
+    }).catch((error) => {
+        res.status(404).json({
+            error: error
+        })
     })
-    .catch((error) =>{
-        console.log(error);
+}
+
+exports.createAPost = (req, res, next) => {
+    const postObj = req.body;
+    const post = new Post({
+        userId: postObj.userId,
+        postTitle: postObj.postTitle,
+        postContent: postObj.postContent,
+        media: postObj.media
+    });
+    post.save().then(() => {
+        res.status(201).json({
+            message: 'Post created successfully!'
+        });
+    }).catch((error) => {
+        res.status(500).json({
+            error: error
+        });
+    });
+};
+
+exports.deletePost = (req, res, next) => {
+    Post.deleteOne({
+        _id: req.params.id
+    }).then((sauce) =>{
+        res.status(200).json({
+            message: 'Post has been successfully deleted!'
+        });
+    }).catch((error) =>{
         res.status(400).json({
             error: error
         })
--- routes/posts.js
+++ routes/posts.js
@@ -7,5 +7,9 @@ const multer = '';
 const postCtrl = require('../controllers/posts')
 
 router.get('/', postCtrl.getAllPosts);
+router.get('/post/:id', postCtrl.getOnePost);
+router.post('/post', postCtrl.createAPost);
+router.delete('post/:id', postCtrl.deletePost);
+
 
 module.exports = router;
