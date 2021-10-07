7c50f5f53517e93b502857b0826e0edcb563020e
Phos23 <phos23@gmail.com>
2021-10-07 19:34:43 +0100

    add cors

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
 
 const userRoutes = require('./routes/user');
 const postRoutes = require('./routes/posts');
 
-//Database
 const db = require('./config/database');
+const app = express();
+app.use(cors());
+app.use(express.json())
 
 db.authenticate()
     .then(() => console.log('Database connected...'))
     .catch(err => console.log('Error: ' + err));
-
-// APP
-const app = express();
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    //Cache-Control, Content-Location, Date, ETag, Expires, Vary
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
 
 // app.get('/', (req, res) => res.send('INDEX'));
 const PORT = process.env.PORT || 5000;
