9025a7c06ff7c5d94572e5ea6260591102065da4
Phos23 <phos23@gmail.com>
2021-10-07 19:39:07 +0100

    remove like route

--- routes/posts.js
+++ routes/posts.js
@@ -11,6 +11,5 @@ router.get('/', postCtrl.getAllPosts);
 router.get('/:id', postCtrl.getOnePost);
 router.post('/', postCtrl.createAPost);
 router.delete('/:id', postCtrl.deletePost);
-router.post('/like/:id', postCtrl.likeAPost);
 
 module.exports = router;
