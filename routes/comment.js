e125379de70f2c15dd47b38f1772dd7168a015b9
Phos23 <phos23@gmail.com>
2021-10-15 14:44:35 +0100

    add new routes

--- routes/comment.js
+++ routes/comment.js
@@ -7,9 +7,9 @@ const multer = '';
 const commentCtrl = require('../controllers/comment');
 
 router.get('/post/:id', commentCtrl.getAllCommentsOfAPost);
-// router.get('/:id', commentCtrl.getOnePost);
+router.delete('/post/:id', commentCtrl.removeAllCommentsOfAPost);
 router.post('/', commentCtrl.createAComment);
-router.put('/:id', commentCtrl.editComment);
+router.put('/', commentCtrl.editComment);
 router.delete('/:id', commentCtrl.deleteComment);
 
 module.exports = router;
