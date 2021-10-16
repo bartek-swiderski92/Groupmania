9a96d0fb088b362a261fc2f00324f2dd3305334f
Phos23 <phos23@gmail.com>
2021-10-16 18:33:17 +0100

    fix if statment and error handling

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,14 +5,14 @@ module.exports = (req, res, next) => {
         const token = req.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, 'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTYzNDMxMjk0MywiaWF0IjoxNjM0MzEyOTQzfQ.ItNXyQddj_arej08iGQYY6uua2xua9hmNfNGk6bzxX8');
         const userId = decodedToken.userId;
-        if (req.body.userId && req.body.userId !== userId) {
+        if (req.body.userId && req.body.userId != userId) {
             throw 'Invalid user ID';
         } else {
             next();
         }
     } catch {
         res.status(401).json({
-            error: new Error('Invalid Request!')
+            error: ('Invalid Request!')
         });
     }
 };
