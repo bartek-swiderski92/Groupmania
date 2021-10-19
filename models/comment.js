269a54492c212a1ca0656a282146b58692d416c1
Phos23 <phos23@gmail.com>
2021-10-19 20:13:09 +0100

    add initial setup

--- models/comment.js
+++ models/comment.js
@@ -1,36 +1,63 @@
-const Sequelize = require('sequelize');
-const db = require('../config/database');
+// const Sequelize = require('sequelize');
+// const db = require('../config/database');
 
 
-const Comment = db.define('comment', {
-    commentId: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        default: 0,
-        unique: true,
-        allowNull: false
-    },
-    userId: {
-        type: Sequelize.INTEGER,
-        allowNull: false
-    },
-    postId: {
-        type: Sequelize.INTEGER,
-        allowNull: false
-    },
-    commentContent: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    media: {
-        type: Sequelize.STRING
-    }
-}, {
-    freezeTableNames: true
-});
-//TODO: belongs to
-// Comment.sync({
-//     force: true
+// const Comment = db.define('comment', {
+//     commentId: {
+//         type: Sequelize.INTEGER,
+//         primaryKey: true,
+//         autoIncrement: true,
+//         default: 0,
+//         unique: true,
+//         allowNull: false
+//     },
+//     userId: {
+//         type: Sequelize.INTEGER,
+//         allowNull: false
+//     },
+//     postId: {
+//         type: Sequelize.INTEGER,
+//         allowNull: false
+//     },
+//     commentContent: {
+//         type: Sequelize.STRING,
+//         allowNull: false
+//     },
+//     media: {
+//         type: Sequelize.STRING
+//     }
+// }, {
+//     freezeTableNames: true
 // });
-module.exports = Comment;
+// //TODO: belongs to
+// // Comment.sync({
+// //     force: true
+// // });
+// module.exports = Comment;
+
+
+module.exports = (sequelize, DataTypes) => {
+    const Comment = sequelize.define("Comment", {
+        commentContent: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        media: {
+            type: DataTypes.STRING
+        }
+    })
+
+    Comment.associate = models => {
+        Comment.belongsTo(models.User, {
+            foreignKey: {
+                allowNull: false
+            }
+        })
+        Comment.belongsTo(models.Post, {
+            foreignKey: {
+                allowNull: false
+            }
+        })
+    }
+    return Comment
+}
--- models/readPost.js
+++ models/readPost.js
@@ -1,30 +1,47 @@
-const Sequelize = require('sequelize');
-const db = require('../config/database');
-const User = require('./user');
-const Post = require('./post');
+// const Sequelize = require('sequelize');
+// const db = require('../config/database');
+// const User = require('./user');
+// const Post = require('./post');
 
 
-const readPost = db.define('readpost', {
-    readPostId: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        default: 0,
-        unique: true,
-        allowNull: false
-    },
-    postId: {
-        type: Sequelize.INTEGER,
-        allowNull: false
-    },
-    userId: {
-        type: Sequelize.INTEGER,
-        allowNull: false
-    }
-}, {
-    freezeTableNames: true
-});
-// readPost.sync({
-//     force: true
+// const readPost = db.define('readpost', {
+//     readPostId: {
+//         type: Sequelize.INTEGER,
+//         primaryKey: true,
+//         autoIncrement: true,
+//         default: 0,
+//         unique: true,
+//         allowNull: false
+//     },
+//     postId: {
+//         type: Sequelize.INTEGER,
+//         allowNull: false
+//     },
+//     userId: {//         type: Sequelize.INTEGER,
+//         allowNull: false
+//     }
+// }, {
+//     freezeTableNames: true
 // });
-module.exports = readPost;
+// // readPost.sync({
+// //     force: true
+// // });
+// module.exports = readPost;
+
+module.exports = (sequelize, DataTypes) => {
+    const readPost = sequelize.define("ReadPost", {})
+
+    readPost.associate = models => {
+        readPost.belongsTo(models.User, {
+            foreignKey: {
+                allowNull: false
+            }
+        })
+        readPost.belongsTo(models.Post, {
+            foreignKey: {
+                allowNull: false
+            }
+        })
+    }
+    return readPost
+}
