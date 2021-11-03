12ffe9d80b66c28327c5d402aed7817254527686
Phos23 <phos23@gmail.com>
2021-11-03 19:24:21 +0000

    added frontend/backend structure

---
+++ frontend/src/components/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import '../styles/App.css';
+import Header from './Header'
+import Footer from './Footer'
+
+function App() {
+    return (
+        <div className="groupmania">
+            <Header />
+            <div>App</div>
+            <Footer />
+        </div>
+
+    )
+
+}
+
+export default App;
