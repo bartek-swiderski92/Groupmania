af70b04e8d41def9da2448ebd5d19024e81de62c
Phos23 <phos23@gmail.com>
2021-11-04 07:47:41 +0000

    add footer hard code

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,6 +1,30 @@
+import '../styles/Footer.css'
+
 function Footer() {
     return (
-        <div>Footer</div>
+        <footer>
+            <div className="links">
+
+                <ul>
+                    <li>link1</li>
+                    <li>link2</li>
+                    <li>link3</li>
+                    <li>link4</li>
+                </ul>
+                <ul>
+                    <li>link1</li>
+                    <li>link2</li>
+                    <li>link3</li>
+                    <li>link4</li>
+                </ul>
+                <ul>
+                    <li>link1</li>
+                    <li>link2</li>
+                    <li>link3</li>
+                    <li>link4</li>
+                </ul>
+            </div>
+        </footer>
     )
 }
 
