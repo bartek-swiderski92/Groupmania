927ba32d098a645512760700edda5d7a46b2e02a
Phos23 <phos23@gmail.com>
2021-11-04 15:59:10 +0000

    add router wip

---
+++ frontend/src/components/Router.js
@@ -0,0 +1,21 @@
+// import React from "react";
+// import {
+//     BrowserRouter as Router,
+//     Switch,
+//     Route,
+//     Link
+// } from "react-router-dom";
+
+// function Router() {
+//     return (
+//         <BrowserRouter>
+//             <Switch>
+//                 <Route exact path='/' component={App} />
+//                 <Route path='/home' component={App} />
+//             </Switch>
+//         </BrowserRouter>
+//     )
+
+// }
+
+// export default Router;
