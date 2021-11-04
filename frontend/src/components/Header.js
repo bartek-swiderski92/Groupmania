af49e2e3b88e652b9ed3e14c5b7ec1fa07427b5f
Phos23 <phos23@gmail.com>
2021-11-04 11:57:06 +0000

    add button, change display to flex

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,13 @@
 import '../styles/Header.css'
+import Button from './Button';
 
-function Header () {
-    return(
+function Header() {
+    return (
         <header>
             <h1>Groupmania</h1>
+            <Button buttonContent="Logout" className='header' />
         </header>
-        )
+    )
 }
 
 export default Header
