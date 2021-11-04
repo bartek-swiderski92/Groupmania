2b05e68ebab321d6a2fa8a9d0be7ddfe42bedb1d
Phos23 <phos23@gmail.com>
2021-11-04 16:33:39 +0000

    add export api

---
+++ frontend/src/main.js
@@ -0,0 +1,16 @@
+const apiUrl = `http://localhost:5000/api`;
+
+// export const postsApi = `${api}/posts`;
+// export const usersApi = `${api}/users`;
+// export const likeApi = `${api}/like`;
+// export const commentApi = `${api}/comment`;
+// export const readPost = `${api}/readPost`;
+
+export const api = {
+    postsApi: `${apiUrl}/posts`,
+    usersApi: `${apiUrl}/users`,
+    likeApi: `${apiUrl}/like`,
+    commentApi: `${apiUrl}/comment`,
+    readPost: `${apiUrl}/readPost`
+}
+
