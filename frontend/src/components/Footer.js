f7e1cf1582f53b10a9132abba22e5a8a969a228b
Phos23 <phos23@gmail.com>
2021-11-05 22:03:55 +0000

    add dynamic data to footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,28 +1,23 @@
-import '../styles/Footer.css'
+import footerLinks from '../data/footer-links'
+import '../styles/Footer.css';
 
 function Footer() {
     return (
         <footer>
-            <div className="links">
+            <div className="footer-container">
+                {footerLinks.map((object, id) => (
+                    <div className="footer-list-wrapper">
+                        <div className="footer-container__title">
+                            {object.name}
+                        </div>
+                        <ul className='footer-container__list' key={object.name + '-' + id + 'ul'}>
+                            {object.elements.map((element, id) => (
+                                <li className="footer-container__link" key={element.name + '-' + id}><a href={element.href}>{element.name}</a></li>
+                            ))}
+                        </ul>
+                    </div>
 
-                <ul>
-                    <li>link1</li>
-                    <li>link2</li>
-                    <li>link3</li>
-                    <li>link4</li>
-                </ul>
-                <ul>
-                    <li>link1</li>
-                    <li>link2</li>
-                    <li>link3</li>
-                    <li>link4</li>
-                </ul>
-                <ul>
-                    <li>link1</li>
-                    <li>link2</li>
-                    <li>link3</li>
-                    <li>link4</li>
-                </ul>
+                ))}
             </div>
         </footer>
     )
