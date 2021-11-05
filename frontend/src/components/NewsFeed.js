f72aec05872e86d7acf4b8cbeab89b39bb941169
Phos23 <phos23@gmail.com>
2021-11-05 14:19:58 +0000

    add welcome message, NewPost component

--- frontend/src/components/NewsFeed.js
+++ frontend/src/components/NewsFeed.js
@@ -1,6 +1,6 @@
-import Post from './Post';
 import { api, getContent } from '../main';
-
+import Post from './Post';
+import NewPost from './NewPost';
 import '../styles/NewsFeed.css';
 
 function NewsFeed() {
@@ -16,6 +16,8 @@ function NewsFeed() {
     getPosts(api.posts)
     return (
         <>
+            <h2>Welcome Back!</h2>
+            <NewPost />
             <Post likes='24' />
             <Post likes='14' />
             <Post likes='124' />
