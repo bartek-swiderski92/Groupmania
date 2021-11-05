a450d14c5411bfa82b75c165eb7da395d7ae5113
Phos23 <phos23@gmail.com>
2021-11-05 22:04:34 +0000

    comment unused code

--- frontend/src/components/NewsFeed.js
+++ frontend/src/components/NewsFeed.js
@@ -1,19 +1,19 @@
-import { api, getContent } from '../main';
+// import { api, getContent } from '../main';
 import Post from './Post';
 import NewPost from './NewPost';
 import '../styles/NewsFeed.css';
 
 function NewsFeed() {
-    let Posts = []
-    async function getPosts(query) {
-        await getContent(query).then(items => {
-            Posts = [...items]
-            console.log(Posts);
-        }).catch((error) => {
-            console.log(error);
-        })
-    }
-    getPosts(api.posts)
+    // let Posts = []
+    // async function getPosts(query) {
+    //     await getContent(query).then(items => {
+    //         Posts = [...items]
+    //         console.log(Posts);
+    //     }).catch((error) => {
+    //         console.log(error);
+    //     })
+    // }
+    // getPosts(api.posts)
     return (
         <div className="news-feed-wrapper">
             <h2>Welcome back!</h2>
