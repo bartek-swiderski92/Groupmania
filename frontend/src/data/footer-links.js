85e80c3279087d128c2f65c12e526b31e9ff5939
Phos23 <phos23@gmail.com>
2021-11-05 22:04:12 +0000

    create  footer-links data file

---
+++ frontend/src/data/footer-links.js
@@ -0,0 +1,52 @@
+const support = {
+    name: 'Support',
+    elements: [
+        {
+            name: 'Contact us',
+            href: '#'
+        }, {
+            name: 'FAQ',
+            href: '#'
+        }, {
+            name: 'Help',
+            href: '#'
+        },
+        {
+            name: 'About Us',
+            href: '#'
+        }]
+}
+
+const opportunities = {
+    name: 'Opportunities',
+    elements: [{
+        name: 'Careers',
+        href: '#'
+    }, {
+        name: 'Become a partner',
+        href: '#'
+    }, {
+        name: 'Apprenticeship',
+        href: '#'
+    }]
+}
+
+const more = {
+    name: 'More',
+    elements: [{
+        name: 'Cookies',
+        href: '#'
+    }, {
+        name: 'Terms of use',
+        href: '#'
+    }, {
+        name: 'Privacy Policy',
+        href: '#'
+    }, {
+        name: 'Store',
+        href: '#'
+    }]
+}
+
+const footerLinks = [support, opportunities, more]
+export default footerLinks
