cc079af3bf2fc89e99cab771aa78ab529e8d3739
Phos23 <phos23@gmail.com>
2021-11-05 22:04:57 +0000

    add listeners

--- frontend/src/components/NewPost.js
+++ frontend/src/components/NewPost.js
@@ -2,14 +2,24 @@ import '../styles/NewPost.css'
 import Button from './Button'
 
 function NewPost() {
+    function attachImage(e) {
+        e.preventDefault();
+        console.log('attaching');
+    }
+    function submitPost(e) {
+        e.preventDefault();
+        console.log('submitting');
+    }
+
     return (
         <div className="new-post-component">
             <h3>Tell us what's on your mind today!</h3>
             <form action="create-post" className="post-body">
-                <input type="text" id="post-title" placeholder="Post Title" className="new-post-input"/>
-                <textarea placeholder="Post Content" className="new-post-input"/>
-                <Button buttonContent='Click here to add a picture' className='new-post' />
-                <Button buttonContent='Add Post' className='new-post' />
+                <input type="text" id="post-title" placeholder="Post Title" className="new-post-input" />
+                <textarea placeholder="Post Content" className="new-post-input" />
+                <Button onClick={attachImage} buttonContent='Click here to add a picture' className='new-post' />
+                <input type="text" id="image-url" placeholder="Image path..." className="new-post-input" />
+                <Button onClick={submitPost} buttonContent='Add Post' className='new-post' />
             </form>
 
         </div>
