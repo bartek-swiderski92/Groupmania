83b2aea9d2ea794e94b3614ed340bd15c95734fb
Phos23 <phos23@gmail.com>
2021-11-06 14:32:48 +0000

    add initial router

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,14 +1,23 @@
 // import logo from './logo.svg';
 import '../styles/App.css';
+import React from 'react'
 import Header from './Header'
-// import Login from './Login'
+import Login from './Login'
 import NewsFeed from './NewsFeed';
-import Footer from './Footer'
+import Footer from './Footer';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
     return (
         <div className="groupmania">
             <Header />
+            <BrowserRouter>
+                <Switch>
+                    <React.Suspense fallback={<span>Loading...</span>} />
+                    <Route path="/login" component={Login} />
+                </Switch>
+            </BrowserRouter>
+
             <div className="main-content">
                 {/* <Login /> */}
                 <NewsFeed />
