bec0cd2ff4eb4b1861c705d3ad8515b8123b7bd1
Phos23 <phos23@gmail.com>
2021-11-07 14:12:37 +0000

    add keys

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,13 +6,13 @@ function Footer() {
         <footer>
             <div className="footer-container">
                 {footerLinks.map((object, id) => (
-                    <div className="footer-list-wrapper">
+                    <div key={object.name + '-' + id + '-wrapper'} className="footer-list-wrapper">
                         <div className="footer-container__title">
                             {object.name}
                         </div>
-                        <ul className='footer-container__list' key={object.name + '-' + id + 'ul'}>
+                        <ul className='footer-container__list' key={object.name + '-' + id + '-ul'}>
                             {object.elements.map((element, id) => (
-                                <li className="footer-container__link" key={element.name + '-' + id}><a href={element.href}>{element.name}</a></li>
+                                <li className="footer-container__link" key={element.name + '-' + id + '-li'}><a key={element.name + '-' + id + '-a'} href={element.href}>{element.name}</a></li>
                             ))}
                         </ul>
                     </div>
