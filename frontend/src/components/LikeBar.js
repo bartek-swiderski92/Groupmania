382e3bf1026f205f3593c23bd428f27513dfb7b0
Phos23 <phos23@gmail.com>
2021-11-07 20:35:49 +0000

    add if else for number of likes

--- frontend/src/components/LikeBar.js
+++ frontend/src/components/LikeBar.js
@@ -6,7 +6,16 @@ function LikeBar({ likes }) {
     return (
         <div className="like-bar">
             <Button className="like" buttonContent="Like!" />
-            <span className="like-bar__number">{likes ? likes + ' People like it!' : 'Be first to like it!'}</span>
+            {/* <span className="like-bar__number">{likes ? likes + ' People like it!' : 'Be first to like it!'}</span> */}
+            <span className="like-bar__number">{(() => {
+                if (!likes) {
+                    return 'Be first to like it!'
+                } else if (likes === 1) {
+                    return 'One Person likes it!'
+                } else {
+                    return likes + ' people like it!'
+                }
+            })()}</span>
         </div>
     )
 }
