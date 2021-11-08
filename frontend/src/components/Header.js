a360601c5e8d764dcd0df8b8dec8fdb79fb32fbb
Phos23 <phos23@gmail.com>
2021-11-08 18:28:54 +0000

    add responsivnes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,16 @@
+import { appUrl } from '../main'
 import '../styles/Header.css'
+import '../styles/Main.css'
 import Button from './Button';
 
 function Header() {
     return (
         <header>
-            <h1>Groupmania</h1>
-            <Button buttonContent="Logout" className='header' />
+            <h1><a className="logo link" href={appUrl}>Groupmania</a></h1>
+            <div class="button-conteiner">
+                <Button buttonContent="My profile" className='header' />
+                <Button buttonContent="Logout" className='header' />
+            </div>
         </header>
     )
 }
