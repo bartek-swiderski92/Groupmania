a6408a7ccc0e8ad82f1930ba1449e1a12080d4a8
Phos23 <phos23@gmail.com>
2021-11-08 13:28:34 +0000

    add dynamic data to user profile

--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -1,14 +1,18 @@
 import '../styles/UserProfile.css';
 import { useEffect, useState } from 'react';
 import { getUserDetails, api } from '../main'
+import Post from './Post';
 
-function UserProfile({ user }) {
+function UserProfile() {
     const [userDetails, setUser] = useState('');
     useEffect(() => {
-        getUserDetails(api.users + '/' + user).then((res) => {
+        getUserDetails(api.users + '/' + document.URL.split('/')[4]).then((res) => {
             setUser(res)
         })
+        console.log(userDetails)
     }, '')
+    console.log(userDetails)
+
     return (
         <div class="user-profile-wrapper">
             <div class="user-details">
@@ -16,20 +20,28 @@ function UserProfile({ user }) {
                     <div class="user-details-main__picture">
                         <img src={require('../media/default-picture.png').default} alt="profile" />
                     </div>
-                    <div class="user-details-main__name">John Smith</div>
+                    <div class="user-details-main__name">{userDetails.firstName + ' ' + userDetails.secondName}</div>
                 </div>
                 <div class="user-info">
 
-                    Email: email@email.com <br />
-                    Gender: Male <br />
-                    Birthday: 01/01/2020<br />
+                    Email: {userDetails.email} <br />
+                    Gender: {userDetails.gender}  <br />
+                    Birthday: {userDetails.dob} <br />
                 </div>
             </div>
             <div class="user-posts">
-                <div>post</div>
-                <div>post</div>
-                <div>post</div>
-                <div>post</div>
+                {(() => {
+                    if (userDetails.Posts) {
+                        return (userDetails.Posts.map((post) => (
+                            <Post key={'post-' + post.id} post={post} user={post.UserId} displayLikes={false} />
+                        ))
+                        )
+                    } else {
+                        <div>No posts</div>
+                    }
+                })()}
+
+
             </div>
         </div>
     )
