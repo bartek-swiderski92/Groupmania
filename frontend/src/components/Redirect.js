f3718fb34269fb392ac8c42ee36f2af59512190b
Phos23 <phos23@gmail.com>
2021-12-11 20:26:53 +0000

    add logic for redirecting

--- frontend/src/components/Redirect.js
+++ frontend/src/components/Redirect.js
@@ -1,4 +1,19 @@
-function RedirectComponent(){
+import React from 'react'
+import { useHistory } from 'react-router-dom';
+
+
+
+
+function RedirectComponent() {
+    const history = useHistory();
+
+    setTimeout(function () {
+        if (localStorage.getItem('token')) {
+            history.push('/newsfeed')
+        } else {
+            history.push('/login')
+        }
+    }, 3000);
     return (
         <div>Redirecting...</div>
     )
