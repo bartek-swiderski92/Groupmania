8fc29c16fc1c153d535f47e2293cb159c11564f8
Phos23 <phos23@gmail.com>
2021-12-11 20:27:42 +0000

    add dynamic buttons to header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,16 +1,43 @@
 import { appUrl } from '../main'
+// import { useHistory, BrowserRouter, Switch, Route } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+
 import '../styles/Header.css'
 import '../styles/Main.css'
+
 import Button from './Button';
 
-function Header() {
+function Header(userLoggedIn) {
+    console.log(userLoggedIn)
+    const history = useHistory();
+
+    function logout() {
+        localStorage.removeItem('token');
+        localStorage.removeItem('userName');
+
+        history.push('/redirect');
+    }
+
     return (
         <header>
             <h1><a className="logo link" href={appUrl}>Groupmania</a></h1>
-            <div className="button-conteiner">
-                <Button buttonContent="My profile" className='header' />
-                <Button buttonContent="Logout" className='header' />
-            </div>
+            {(() => {
+                if (userLoggedIn === 1) {
+                    return (
+                        <div>
+                            <div className="button-container">
+                                <Button buttonContent="My profile" className='header' />
+                                <Button buttonContent="Logout" onClick={logout} className='header' />
+                            </div>
+                        </div>
+                    )
+                } else if (userLoggedIn === 0) {
+                    return <button>BUTTON</button>
+                }
+            })()
+            }
         </header>
     )
 }
