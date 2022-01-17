bf56af6b36696b850d612d4251a5264d317df94f
Phos23 <phos23@gmail.com>
2022-01-17 18:53:46 +0000

    add submitPost listener

--- frontend/src/components/NewPost.js
+++ frontend/src/components/NewPost.js
@@ -1,25 +1,27 @@
-import '../styles/NewPost.css'
+import axios from 'axios';
+
 import Button from './Button'
+import '../styles/NewPost.css'
 
 function NewPost() {
     // function attachImage(e) {
     //     e.preventDefault();
     //     console.log('attaching');
     // }
-    function submitPost(e) {
-        e.preventDefault();
-        console.log('submitting');
+    function submitPost(event) {
+        event.preventDefault();
+        const [postTitle, postContent, postMedia] = event.targets.elements
     }
 
     return (
         <div className="new-post-wrapper">
             <h3>Tell us what's on your mind today!</h3>
-            <form action="create-post" className="post-body">
+            <form action="create-post" className="post-body" onSubmit={submitPost}>
                 <input type="text" id="post-title" placeholder="Post Title" className="new-post-input" />
                 <textarea placeholder="Post Content" className="new-post-input" />
-                <input type="file" id="image-url-new-post" placeholder="Image path..." className="new-post-input"/>
+                <input type="file" id="image-url-new-post" placeholder="Image path..." className="new-post-input" />
                 {/* <Button type="file" className='new-post__button' onClick={attachImage} buttonContent='Click here to add a picture' /> */}
-                <Button className='new-post__button' onClick={submitPost} buttonContent='Add Post' />
+                <Button type='submit' className='new-post__button' buttonContent='Add Post' />
             </form>
 
         </div>
