e4567f61dd92e74a0d53b623c088481445c699b6
Phos23 <phos23@gmail.com>
2022-01-21 13:45:25 +0000

    add likes state

--- frontend/src/components/LikeBar.js
+++ frontend/src/components/LikeBar.js
@@ -1,19 +1,35 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import { api, apiUrl, getPosts } from '../main';
 import '../styles/LikeBar.css'
 import Button from './Button';
 
 
 function LikeBar({ likes }) {
+    const [getLikes, setLikes] = useState([]);
+    useEffect(() => {
+        setLikes(likes.map(like => like.id))
+    })
+
+
+    function submitLike(event) {
+        event.preventDefault();
+        console.log('submitting like');
+        axios.post({})
+    }
+
     return (
         <div className="like-bar">
-            <Button className="like" buttonContent="Like!" />
+            <Button onClick={submitLike} className="like" buttonContent="Like!" />
             {/* <span className="like-bar__number">{likes ? likes + ' People like it!' : 'Be first to like it!'}</span> */}
             <span className="like-bar__number">{(() => {
                 if (!likes) {
                     return 'Be first to like it!'
-                } else if (likes === 1) {
+                } else if (likes.length === 1) {
                     return 'One Person likes it!'
                 } else {
-                    return likes + ' people like it!'
+                    return likes.length + ' people like it!'
                 }
             })()}</span>
         </div>
