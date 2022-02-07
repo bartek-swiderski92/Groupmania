2f21e0658facc06e9c5fe64ef4bae1d9207aeb63
Phos23 <phos23@gmail.com>
2022-02-07 16:31:18 +0000

    wip listener for component on screen

--- frontend/src/components/NewsFeed.js
+++ frontend/src/components/NewsFeed.js
@@ -5,32 +5,20 @@ import { useEffect, useState } from 'react';
 import NewPost from './NewPost';
 import '../styles/NewsFeed.css';
 
-// async function getUsers(query) {
-//     // let Posts = []
-//     return await getContent(query).then(items => {
-//         console.log(items);
-//         return [...items]
-//     }).catch((error) => {
-//         console.log(error);
-//     })
-// }
-
-// function ShowPost({ post }) {
-//     return (
-//         <div>
-//             {Array.isArray(post) ? (
-
-//                 post.map((singlePost) => {
-//                     return <Post key={'post-' + singlePost.id} post={singlePost} user={singlePost.UserId} />
-//                 })
-//             ) : <Post key={'post-' + post.id} post={post} user={post.UserId} />
-//             }
-//         </div>
-//     )
-// }
-
 function NewsFeed(props) {
     const history = useHistory();
+
+    const [readPostsWhileScroll, setReadPostWhileScroll] = useState([])
+
+    function isInViewport(element) {
+        const rect = element.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
 
     const [posts, setPosts] = useState([]);
     useEffect(() => {
@@ -38,9 +26,27 @@ function NewsFeed(props) {
             setPosts(res)
         })
     }, [])
-    if (props.userLoggedIn !== true){
-    history.pushState('/login')
+
+    if (props.userLoggedIn !== true) {
+        history.pushState('/login')
     }
+
+
+
+    function readPostWhileScrolling() {
+        const unreadPosts = document.querySelectorAll('.post-wrapper--unread')
+        unreadPosts.forEach((unreadPost) => {
+            if (isInViewport(unreadPost)) {
+                // console.log(unreadPost.getAttribute('data-post-id'))
+                setReadPostWhileScroll([...unreadPost.getAttribute('data-post-id')])
+                console.log(readPostsWhileScroll)
+            }
+        })
+
+    }
+    //TODO: finish ticking posts as read when on screen
+    // window.addEventListener('scroll', readPostWhileScrolling)
+
     return (
         <div className="news-feed-wrapper">
             <h2>Welcome back {localStorage.getItem('userName')}!</h2>
