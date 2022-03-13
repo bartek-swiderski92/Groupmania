3178287fae0e26f9b1d42ccc344cba7a93be9cd0
Phos23 <phos23@gmail.com>
2022-03-13 15:39:36 +0000

    add displaying images in FE

--- backend/app.js
+++ backend/app.js
@@ -59,6 +59,8 @@ app.use('/api/likes', likeRoutes);
 app.use('/api/comments', commentRoutes);
 app.use('/api/readPosts', readPostRoutes);
 
+app.use('/media', express.static(path.join(__dirname, 'media')));
+
 app.use(express.json())
 
 app.use(express.urlencoded({
