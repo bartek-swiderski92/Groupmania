644940cd4c42be3dca539ff7edb041e60f639481
Phos23 <phos23@gmail.com>
2022-03-16 14:56:33 +0000

    add deletePicture route

--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -9,7 +9,7 @@ const commentCtrl = require('../controllers/comment');
 router.get('/post/:id', auth, commentCtrl.getAllCommentsOfAPost);
 router.post('/', auth, multer, commentCtrl.createAComment);
 router.put('/:id', auth, multer, commentCtrl.editComment);
-// router.delete('/picture/:id', auth, commentCtrl.deletePicture);
+router.delete('/picture/:id', auth, commentCtrl.deletePicture);
 router.delete('/:id', auth, commentCtrl.deleteComment);
 
 module.exports = router;
